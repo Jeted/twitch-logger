c1ca94b5b9f93d6bd2441fcbe9495473a23e3ed5
Jeted <jeted@pm.me>
2021-11-12 03:02:00 +0100

    Add all used events

--- src/twitch/index.ts
+++ src/twitch/index.ts
@@ -1,5 +1,5 @@
-import logger from '../misc/logger';
 import { ChatClient } from '@twurple/chat';
+import logger from '../misc/logger';
 
 const chat = new ChatClient({
   channels: ['jeted'],
@@ -10,11 +10,27 @@ const chat = new ChatClient({
   requestMembershipEvents: true,
 });
 
+chat.onMessage(async (channel, user, message, tags) => {});
+
+chat.onSub(async (channel, user, chatSubInfo, tags) => {});
+
+chat.onResub(async (channel, user, chatSubInfo, tags) => {});
+
+chat.onSubGift(async (channel, user, chatSubInfo, tags) => {});
+
+chat.onMessageRemove(async (channel, message, tags) => {});
+
+chat.onTimeout(async (channel, user, duration) => {});
+
+chat.onBan(async (channel, user) => {});
+
 chat.onJoin((channel, user) => {
   if (user.includes('justinfan')) {
     logger.info(`Twitch: Connected ${channel}`);
   }
 });
+
+chat.onPart((channel, user) => {});
 
 class Twitch {
   async connect() {
