b9c28b77290f2dc87e9c0874a900b28076cdf3af
Jeted <jeted@pm.me>
2021-11-18 22:22:39 +0100

    Update `getUserByLogin` for future consistency

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
+import { URL } from 'url';
 import config from '../config';
 import logger from '../misc/logger';
 import { User, UserResponse } from '../misc/interfaces';
 
 export async function getUserByLogin(login: string): Promise<User | undefined> {
-  const url = `${config.API_BASE_URL}/helix/users?login=${login}`;
+  const url = new URL(`${config.API_BASE_URL}/helix/users`);
+  url.searchParams.append('login', login);
 
   try {
-    const { data } = await axios.get<UserResponse>(url);
+    const { data } = await axios.get<UserResponse>(url.toString());
     const user = data.data[0];
 
     if (!user) return undefined;
